48a57ffdbcaeff5bd379994301e35b7c203a90e4
itzajaykr <106441968+itzajaykr@users.noreply.github.com>
2024-07-17 11:36:31 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+let myName="Ajay";
+console.log(myName);
+let myAge=24
+let sessionId=true;
+let myState;
+let myCountry=null;
+null;
+console.table([myName,myAge,sessionId,myState,myCountry]);
+console.log(typeof"true")
+console.log(typeof"null")
+console.log(typeof"sessionId")
